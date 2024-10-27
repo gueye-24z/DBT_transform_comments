7b0fdac21abcd2843c67f051d8fcb8d4860b33a0
Gueye <mohamed.ge21@gmail.com>
2024-10-27 20:56:40 +0100

    fix: création de modèles incrémentaux pour n'intégrer que les nouvelles données

--- models/silver/silver_comments.sql
+++ models/silver/silver_comments.sql
@@ -1,7 +1,11 @@
-{{ config(enabled=true) }}
+{{ config(
+    materialized='table',
+    enabled=true
+) }}
 
 with judgeme_reviews as (
     select 
+        cast(COMMENT_ID_FROM_SOURCE as varchar) as COMMENT_ID_FROM_SOURCE,  -- Utilisation de COMMENT_ID_FROM_SOURCE comme clé provenant de Judge.me
         cast(review_title as varchar) as review_title,
         cast(review_text as varchar) as review_text,
         cast(review_date as date) as review_date,
@@ -12,30 +16,30 @@ with judgeme_reviews as (
 
 csv_reviews as (
     select
+        cast(COMMENT_ID_FROM_SOURCE as varchar) as COMMENT_ID_FROM_SOURCE,  -- Utilisation de COMMENT_ID_FROM_SOURCE comme clé provenant de CSV
         cast(review_title as varchar) as review_title,
         cast(review_text as varchar) as review_text,
         cast(review_date as date) as review_date,
         cast(author as varchar) as author,
         null as product_title  -- Si `product_title` n'existe pas dans `csv`, on le met à null
     from {{ ref('stg_csv_S3_comments') }}
+),
+
+-- Fusion des deux sources avec ajout d'un ID auto-incrémental
+combined_reviews as (
+    select
+        row_number() over() as id,  -- Génération d'un identifiant unique auto-incrémental
+        COMMENT_ID_FROM_SOURCE,
+        review_title,
+        review_text,
+        review_date,
+        author,
+        product_title
+    from (
+        select * from judgeme_reviews
+        union all
+        select * from csv_reviews
+    )
 )
 
-select
-    review_title,
-    review_text,
-    review_date,
-    author,
-    product_title
-from judgeme_reviews
-
-union all
-
-select
-    review_title,
-    review_text,
-    review_date,
-    author,
-    product_title
-from csv_reviews
-
-
+select * from combined_reviews
--- models/staging/stg_csv_S3_comments.sql
+++ models/staging/stg_csv_S3_comments.sql
@@ -1,14 +1,22 @@
--- stg_csv_S3_comments.sql
-{{ config(enabled=true) }}
-
+{{ config(
+    materialized='incremental',
+    unique_key='COMMENT_ID_FROM_SOURCE',
+    enabled=true
+) }}
 
 with raw_data as (
     select
+        REVIEW_ID as COMMENT_ID_FROM_SOURCE,  -- Renommage de REVIEW_ID en COMMENT_ID_FROM_SOURCE
         REVIEW_TITLE as review_title,
         REVIEW_TEXT as review_text,
         REVIEW_DATE as review_date,
         AUTHOR_TITLE as author
-    from {{ source('csv_S3', 'judgeme_reviews') }}  -- Nom de votre table dans le schéma "csv"
+    from {{ source('csv_S3', 'judgeme_reviews') }}
+    
+    {% if is_incremental() %}
+        -- Filtrer pour récupérer uniquement les nouvelles données basées sur `REVIEW_ID`
+        where REVIEW_ID > (select max(COMMENT_ID_FROM_SOURCE) from {{ this }})
+    {% endif %}
 )
 
 select * from raw_data
